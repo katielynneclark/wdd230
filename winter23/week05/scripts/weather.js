4cde38ac77f2cba52a36ad102fffab4018fe97d3
katielynneclark <cla16011@byui.edu>
2023-02-01 12:33:04 -0700

    Weather Update

--- winter23/week05/scripts/weather.js
+++ winter23/week05/scripts/weather.js
@@ -1,33 +1,24 @@
-const currentTemp = document.querySelector('#current-temp');
-  const weatherIcon = document.querySelector('#weather-icon');
-  const captionDesc = document.querySelector('figcaption');
+const myTown = document.querySelector('#town')
+const myGraphic = document.querySelector('#graphic')
+const myDescription = document.querySelector('#description')
+const myTemperature = document.querySelector('#temperature')
 
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier&units=imperial&appid=8e6510e8d4e1682e9b87719658d8bae2';
+const townName = "Trier"
+const myKey = "8e6510e8d4e1682e9b87719658d8bae2"
 
-  async function apiFetch() {
-    try {
-        const response = await fetch(url);
-        if(response.ok) {
-            const data = await response.json();
-            displayResults(data);
-            console.log(data);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
-    }
-  }
+const myURL = `//api.openweathermap.org/data/2.5/weather?q=${townName}&appid=${myKey}&units=imperial`
 
-  apiFetch();
+fetch(myURL)
+.then((response) => response.json())
+.then((data) => displayData(data));
 
-  function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&degF`;
 
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}10d.png`;
-    let desc = data.weather[0].description;
+function displayData (data) {
+console.log(data)
+myTown.innerHTML = data.name
+myGraphic.src=` http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+myGraphic.alt=data.weather[0].main
+myDescription.innerHTML = data.weather[0].description
+myTemperature.innerHTML = `${data.main.temp}&degF`
 
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = `${desc}`;
-  }
+}
